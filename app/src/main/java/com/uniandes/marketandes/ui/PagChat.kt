37a9ec1e6c345e24b928ded6447482e3ced96ed6
Juan José Sierra <j.sierraa@uniandes.edu.co>
2025-03-18 14:32:24 -0500

    Revert "Authentication"

--- app/src/main/java/com/uniandes/marketandes/ui/PagChat.kt
+++
@@ -1,9 +0,0 @@
-package com.uniandes.marketandes.ui
-
-import androidx.compose.runtime.Composable
-import androidx.compose.material3.Text
-
-@Composable
-fun PagChat() {
-    Text("Página de Chats")
-}
