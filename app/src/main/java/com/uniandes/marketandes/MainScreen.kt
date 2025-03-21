150faa9d4ff66c90c531b985195626e93a9ea574
Daniela Parra <80072127+Daniela-parra@users.noreply.github.com>
2025-03-21 00:08:05 -0500

    Sensor functionality
    GPS from recommended stores

--- app/src/main/java/com/uniandes/marketandes/MainScreen.kt
+++ app/src/main/java/com/uniandes/marketandes/MainScreen.kt
@@ -2,13 +2,25 @@ package com.uniandes.marketandes
 
 import RegisterScreen
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.GridView
+import androidx.compose.material.icons.outlined.History
+import androidx.compose.material.icons.outlined.ShoppingBag
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.outlined.Storefront
 import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -16,14 +28,24 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.NavController
 import com.uniandes.marketandes.ui.*
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.google.android.gms.maps.model.LatLng
 import com.uniandes.marketandes.ui.authentication.ui.AuthenticationScreen
 import com.uniandes.marketandes.ui.authentication.ui.AuthenticationViewModel
 import com.uniandes.marketandes.ui.authentication.ui.RegistrationViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
+    var userLocation by remember { mutableStateOf<LatLng?>(null) }
+
+    GetUserLocation { location ->
+        userLocation = location
+    }
+
     val items = listOf(
         BottomNavItem("", R.drawable.shop_icon, "pag_comprar"),
         BottomNavItem("", R.drawable.add_icon, "pag_vender"),
@@ -35,47 +57,67 @@ fun MainScreen() {
     var selectedIndex by remember { mutableStateOf(items.indexOfFirst { it.route == "pag_home" }) }
     val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
 
-    Scaffold(
-        topBar = { HeaderBar() },
-        modifier = Modifier.fillMaxSize(),
-        bottomBar = {
-            NavigationBar(containerColor = Color(0xFF00296B)) {
-                items.forEach { item ->
-                    NavigationBarItem(
-                        currentRoute == item.route,
-                        onClick = {
-                            selectedIndex = items.indexOf(item)
-                            navController.navigate(item.route) {
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        },
-                        icon = {
-                            Icon(
-                                painter = painterResource(id = item.icon),
-                                contentDescription = item.title,
-                                tint = if (currentRoute == item.route) Color(0xFFFDC500) else Color.White
-                            )
-                        },
-                        label = {},
-                        colors = NavigationBarItemDefaults.colors(
-                            selectedIconColor = Color(0xFFFDC500),
-                            unselectedIconColor = Color.White,
-                            indicatorColor = Color(0xFF00296B)
-                        )
-                    )
-                }
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        gesturesEnabled = drawerState.isOpen,
+        drawerContent = {
+            Box(
+                modifier = Modifier
+                    .requiredHeight(400.dp)
+                    .fillMaxHeight(0.2f)
+                    .requiredSizeIn(maxHeight = 200.dp)
+                    .background(Color(0xFFF7F2FC))
+            )
+
+            if (drawerState.isOpen) {
+                DrawerContent(navController, onClose = { scope.launch { drawerState.close() } })
             }
         }
-    ) { innerPadding ->
-        ContentScreen(navController, Modifier.padding(innerPadding))
+    ) {
+        Scaffold(
+            topBar = { HeaderBar (onMenuClick = { scope.launch { drawerState.open() } } ) },
+            modifier = Modifier.fillMaxSize(),
+            bottomBar = {
+                NavigationBar(containerColor = Color(0xFF00296B)) {
+                    items.forEach { item ->
+                        NavigationBarItem(
+                            currentRoute == item.route,
+                            onClick = {
+                                selectedIndex = items.indexOf(item)
+                                navController.navigate(item.route) {
+                                    launchSingleTop = true
+                                    restoreState = true
+                                }
+                            },
+                            icon = {
+                                Icon(
+                                    painter = painterResource(id = item.icon),
+                                    contentDescription = item.title,
+                                    tint = if (currentRoute == item.route) Color(0xFFFDC500) else Color.White
+                                )
+                            },
+                            label = {},
+                            colors = NavigationBarItemDefaults.colors(
+                                selectedIconColor = Color(0xFFFDC500),
+                                unselectedIconColor = Color.White,
+                                indicatorColor = Color(0xFF00296B)
+                            )
+                        )
+                    }
+                }
+            }
+        ) { innerPadding ->
+            ContentScreen(navController,userLocation, Modifier.padding(innerPadding))
+        }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HeaderBar() {
-    var isSidebarOpen by remember { mutableStateOf(false) }
+fun HeaderBar(onMenuClick: () -> Unit) {
     TopAppBar(
         title = {
             Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
@@ -87,11 +129,11 @@ fun HeaderBar() {
             }
         },
         navigationIcon = {
-            IconButton(onClick = { isSidebarOpen = !isSidebarOpen }) {
+            IconButton(onClick = onMenuClick) {
                 Icon(
                     painter = painterResource(id = R.drawable.settings_logo),
                     contentDescription = "Menú",
-                    tint = if (isSidebarOpen) Color(0xFFFDC500) else Color.White
+                    tint = Color.White
                 )
             }
         },
@@ -103,7 +145,7 @@ fun HeaderBar() {
 }
 
 @Composable
-fun ContentScreen(navController: NavHostController, modifier: Modifier) {
+fun ContentScreen(navController: NavHostController, userLocation: LatLng?, modifier: Modifier) {
     NavHost(
         navController,
         startDestination = "pag_home",  // Aquí empieza tu navegación
@@ -117,6 +159,13 @@ fun ContentScreen(navController: NavHostController, modifier: Modifier) {
         composable("pag_intercambio") { PagIntercambio() }
         composable("pag_chat") { PagChat(navController) }
         composable("pag_compra") { PagComprar(navController) }
+        composable("pag_store_maps") {
+            userLocation?.let { location ->
+                PagStoreMaps(navController)
+            } ?: run {
+                Text("Obteniendo ubicación...", fontSize = 20.sp, modifier = Modifier.padding(16.dp))
+            }
+        }
         composable("detalle_compra/{productName}") { backStackEntry ->
             val productName = backStackEntry.arguments?.getString("productName") ?: ""
             PagCompraDetail(navController, productName)
@@ -137,6 +186,65 @@ fun ContentScreen(navController: NavHostController, modifier: Modifier) {
         composable("confirmarUbicacion/{chatId}/{nombreUbicacion}/{imagenUrl}") {
             ConfirmarUbicacionScreen(navController = navController)
         }
+        composable("ubicaciondetail/{nombreUbicacion}/{imagenUrl}") {
+            UbicacionDetail(navController)
+        }
+
+
     }
 }
 
+@Composable
+fun DrawerContent(navController: NavController, onClose: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .width(280.dp)  // Fuerza el ancho a 280.dp
+            .background(Color(0xFFF7F2FC))
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .height(500.dp)
+                .padding(start = 16.dp, top = 40.dp),
+            verticalArrangement = Arrangement.Top
+        ) {
+            DrawerItem(icon = Icons.Outlined.AccountCircle, text = "Mi perfil")
+            DrawerItem(icon = Icons.Outlined.GridView, text = "Mis publicaciones")
+            DrawerItem(icon = Icons.Outlined.History, text = "Historial")
+            DrawerItem(icon = Icons.Outlined.ShoppingBag, text = "Mis compras")
+            DrawerItem(
+                icon = Icons.Outlined.Storefront,
+                text = "Tiendas recomendadas",
+                onClick = {
+                    navController.navigate("pag_store_maps")
+                    onClose()
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun DrawerItem(icon: ImageVector, text: String, onClick: (() -> Unit)? = null) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .clickable { onClick?.invoke() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = text,
+            tint = Color(0xFF1A237E), // Azul oscuro similar al de la imagen
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.width(16.dp)) // Espaciado entre icono y texto
+        Text(
+            text = text,
+            fontSize = 16.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Medium
+        )
+    }
+}
